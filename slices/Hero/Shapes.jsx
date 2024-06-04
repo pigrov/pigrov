118da0a17db15479f2e115ae8790a49a4c3b4d8b
pigrov <pigrov@gmail.com>
2024-06-04 11:24:54 +0300

    sounds

--- slices/Hero/Shapes.jsx
+++ slices/Hero/Shapes.jsx
@@ -25,7 +25,7 @@ export default function Shapes() {
                         blur={1}
                         far={9}
                     />
-                    <Environment preset="studio" />
+                    <Environment preset="city" />
                 </Suspense>
             </Canvas>
         </div>
@@ -35,13 +35,49 @@ export default function Shapes() {
 function Geometries() {
     const geometries = [
         {
-            position: [0, 0, 0],
+            position: [0, 0, -1],
             r: 0.4,
             geometry: new THREE.IcosahedronGeometry(3),
         },
+        {
+            position: [1, 1, 4],
+            r: 0.4,
+            geometry: new THREE.CylinderGeometry(1, 0.7, 2, 32),
+        },
+        {
+            position: [-1, -1, 2],
+            r: 0.4,
+            geometry: new THREE.TorusGeometry(1, 0.6, 10, 32),
+        },
+        {
+            position: [1, -1, 3],
+            r: 0.4,
+            geometry: new THREE.OctahedronGeometry(1.2),
+        },
+        {
+            position: [-2, 2, -3],
+            r: 0.6,
+            geometry: new THREE.CapsuleGeometry(1, 2, 4, 16),
+        },
     ];
 
-    const materials = [new THREE.MeshNormalMaterial()];
+    const materials = [
+        new THREE.MeshNormalMaterial(),
+        new THREE.MeshStandardMaterial({ color: 0xd33257, roughness: 0 }),
+        new THREE.MeshStandardMaterial({ color: 0x71ba51, roughness: 0.1 }),
+        new THREE.MeshStandardMaterial({ color: 0x71ba51, roughness: 0.2 }),
+        new THREE.MeshStandardMaterial({ color: 0x1dabb8, roughness: 0.3 }),
+        new THREE.MeshStandardMaterial({ color: 0x00b5b5, roughness: 0.4 }),
+        new THREE.MeshStandardMaterial({ color: 0x422e39, metalness: 1, roughness: 0.5 }),
+        new THREE.MeshStandardMaterial({ color: 0x3e4651, metalness: 1, roughness: 0.6 }),
+        new THREE.MeshStandardMaterial({ color: 0xe3000e, metalness: 1, roughness: 0.7 }),
+    ];
+
+    const soundEffects = [
+        new Audio("/sounds/impact1.ogg"),
+        new Audio("/sounds/impact2.ogg"),
+        new Audio("/sounds/impact3.ogg"),
+    ];
 
     return geometries.map(({ position, r, geometry }) => (
         <Geometry
@@ -50,13 +86,14 @@ function Geometries() {
             r={r}
             geometry={geometry}
             materials={materials}
+            soundEffects={soundEffects}
         />
     ));
 }
 
-function Geometry({ position, r, geometry, materials }) {
+function Geometry({ position, r, geometry, materials, soundEffects }) {
     const ref = useRef();
-    const [visible, setVisible] = useState(true);
+    const [visible, setVisible] = useState(false);
 
     const baseMaterial = getRandomMaterial();
 
@@ -66,6 +103,8 @@ function Geometry({ position, r, geometry, materials }) {
 
     function handleClick(e) {
         const mesh = e.object;
+
+        gsap.utils.random(soundEffects).play();
 
         gsap.to(mesh.rotation, {
             x: `+=${gsap.utils.random(0, 3)}`,
@@ -84,6 +123,23 @@ function Geometry({ position, r, geometry, materials }) {
     const handlePointerOut = () => {
         document.body.style.cursor = "default";
     };
+
+    useEffect(() => {
+        let ctx = gsap.context(() => {
+            setVisible(true);
+            gsap.from(ref.current.scale, {
+                x: 0,
+                y: 0,
+                z: 0,
+                duration: 1.5,
+                ease: "elastic.out(1, 0.3)",
+                yoyo: true,
+                delay: 1,
+            });
+        });
+
+        return () => ctx.revert();
+    }, []);
 
     return (
         <group position={position} ref={ref}>
